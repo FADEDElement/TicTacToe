6c69eb5064ad516f99bbd3c2e915edadc1bb1d84
FADEDElement <elementplayzgames@gmail.com>
2022-12-25 23:19:18 -0600

    rewrote program for optimization

---
+++ README.md
@@ -0,0 +1,48 @@
+# Tic Tac Toe
+
+Simple terminal based tic tac toe game.
+
+## Description
+
+A game in which two players alternately put X's and O's in boxes of a figure formed by two vertical lines crossing two horizontal lines and each tries to get a row of three X's or three O's before the opponent does.
+
+## Getting Started
+
+### Installing
+
+* Download Here: [Releases](https://github.com/FADEDElement/TicTacToe/releases)
+
+### Executing program
+
+Option 1:
+* Double Click the "tictactoe.py" file
+
+Option 2:
+* Open the command line and cd into download location folder
+* Type the command below:
+```
+python tictactoe.py
+```
+
+## What I used / learned
+* Dictionaries
+* Functions
+* While Loops
+* Lists
+
+## Authors
+
+Contributors names and contact info
+
+[@FADEDElement](https://www.youtube.com/c/FADEDElement)
+
+## Version History
+
+* 2.0
+    * Major code optimizations
+* 1.0
+    * Initial Release
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details.
---
+++ tictactoe.py
@@ -0,0 +1,77 @@
+import os
+import re
+import random
+
+playing = True
+
+def displayBoard():
+    os.system("cls")
+
+    print(f"""
+         {awnsers[0]} | {awnsers[1]} | {awnsers[2]}
+        ---+---+---
+         {awnsers[3]} | {awnsers[4]} | {awnsers[5]}
+        ---+---+---
+         {awnsers[6]} | {awnsers[7]} | {awnsers[8]}
+    """)
+
+def translateLocation (location):
+    if location == "tl": return 0
+    elif location == "tm": return 1
+    elif location == "tr": return 2
+    elif location == "ml": return 3
+    elif location == "m": return 4
+    elif location == "mr": return 5
+    elif location == "bl": return 6
+    elif location == "bm": return 7
+    elif location == "br": return 8
+
+def gameState(awnsersTemp):
+    temp = " "
+    winner = ""
+    players = ["X", "O"]
+    awnsersTemp = temp.join(awnsersTemp).replace(" ", "")
+
+    for player in players:
+        if (re.match(f"{player}{player}{player}[\S][\S][\S][\S][\S][\S]", awnsersTemp) or
+            re.match(f"[\S][\S][\S]{player}{player}{player}[\S][\S][\S]", awnsersTemp) or
+            re.match(f"[\S][\S][\S][\S][\S][\S]{player}{player}{player}", awnsersTemp) or
+            re.match(f"{player}[\S][\S]{player}[\S][\S]{player}[\S][\S]", awnsersTemp) or
+            re.match(f"[\S]{player}[\S][\S]{player}[\S][\S]{player}[\S]", awnsersTemp) or
+            re.match(f"[\S][\S]{player}[\S][\S]{player}[\S][\S]{player}", awnsersTemp) or
+            re.match(f"{player}[\S][\S][\S]{player}[\S][\S][\S]{player}", awnsersTemp) or
+            re.match(f"[\S][\S]{player}[\S]{player}[\S]{player}[\S][\S]", awnsersTemp)
+        ): winner = player
+        elif "-" not in awnsersTemp: winner = "null"
+
+        if winner == player:
+            if not input (player + ", congradulations you win!\nWould you like to play again? [y/n] ") == "y" or "Y": playing = False
+            else: winner = ""
+        elif winner == "null":
+            if not input ("Draw!\nWould you like to play again? [y/n] ") == "y" or "Y": playing = False
+            else: winner = ""
+
+playing = True
+
+while True:
+    turn = "X"
+    playableLocations = ""
+    awnsers = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+    displayBoard()
+
+    while playing:
+        playablePosition = input(f"{turn} -> Please enter a location: ")
+
+        if translateLocation(playablePosition) == -1 or not awnsers[translateLocation(playablePosition)] == "-":
+            displayBoard()
+            continue
+
+        awnsers[translateLocation(playablePosition)] = turn
+        
+        if turn == "X": turn = "O"
+        elif turn == "O": turn = "X"
+
+        displayBoard()
+        gameState(awnsers)
+    
+    break
